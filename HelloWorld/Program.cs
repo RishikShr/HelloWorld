bd434398e5e5e88c0f0601dc8ddb073afa413c45
RISHIK SHRIVASTAV <rishikshrivastav0@gmail.com>
2023-01-10 14:18:54 +0530

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+ namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string : ");
+            string str = Console.ReadLine();
+            LengthHelper lp = new LengthHelper();
+            Console.WriteLine("Length of the string is : " + lp.findLength(str));
+            Console.ReadKey();
+        }
+
+        public class LengthHelper
+        {
+            public int findLength(string s)
+            {
+                if (s == null)
+                {
+                    throw new ArgumentException("string  cannot be null");
+                }
+                return s.Length;
+            }
+        }
+    }
+}
